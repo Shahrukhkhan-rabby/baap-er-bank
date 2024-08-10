b5b3248e1638e5e70d88dfeebf72f31bf6156e8b
Shahrukhkhan-rabby <shahrukhkhan3418198@gmail.com>
2024-08-10 10:52:07 +0600

    25-4

--- js/bank.js
+++ js/bank.js
@@ -1,4 +1,5 @@
 document.getElementById('login-submit').addEventListener('click', function(){
+    // get user email
     const emailField = document.getElementById('user-email');
     const userEmail = emailField.value;
     console.log(userEmail);
@@ -6,7 +7,7 @@ document.getElementById('login-submit').addEventListener('click', function(){
     const passwordField = document.getElementById('user-password');
     const userPassword = passwordField.value;
     console.log(userPassword);
-
+// check email and password
     if(userEmail == 'sontan@baap.com' && userPassword == 'secret'){
         window.location.href = 'banking.html'
     }
