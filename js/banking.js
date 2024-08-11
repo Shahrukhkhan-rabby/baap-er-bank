476bb1bfe1d45e99ff133c047cb8b0be5391b233
Shahrukhkhan-rabby <shahrukhkhan3418198@gmail.com>
2024-08-11 06:04:34 +0600

    conclued

--- js/banking.js
+++ js/banking.js
@@ -27,7 +27,30 @@ document.getElementById('deposit-button').addEventListener('click', function(){
 
     // cleat the deposit input field
     depositInput.value = '';
+});
+
+// handle withdraw event handler 
+
+document.getElementById('withdraw-button').addEventListener('click', function(){
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmountText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(withdrawAmountText);
+    console.log(newWithdrawAmount);
+
+    // set withdraw total
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawText);
+
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotal.innerText = newWithdrawTotal;
+
+    const balanceTotal = document.getElementById('balance-total');
+     const previousBalanceText = balanceTotal.innerText;
+     previousBalanceTotal = parseFloat(previousBalanceText);
+     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+
+     balanceTotal.innerText = newBalanceTotal;
+
+    withdrawInput.value = '';
 })
-
-
-
